ab22eb45880b57313c0bf528bf37a3bbf7cd3f32
Alexand Sorokin <bsorokind@mail.ru>
2012-07-23 19:32:40 +0300

    bugfix

--- salest/cart/forms.py
+++ salest/cart/forms.py
@@ -5,6 +5,9 @@
 from django.forms import ModelForm, forms
 
 from salest.cart.models import CartItem
+from copy import copy
+from salest.discounts.models import Discount
+from salest.discounts.discounts import CartCodeMinOrderDiscount
 
 
 class CartItemForm(ModelForm):
@@ -13,15 +16,33 @@ class CartItemForm(ModelForm):
     class Meta:
         model = CartItem
         fields = ('quantity',)
+        
+    
+    def save(self, *args, **kwargs):
+        instance = super(CartItemForm, self).save(*args, **kwargs)
+        cart = instance.cart
+        discounts = instance.cart.discount.all()
+        if discounts:
+            for discount in discounts:
+                disc = CartCodeMinOrderDiscount()
+                if not disc.is_valid(discount.code, cart.get_items_price()):
+                    cart.discounts.remove(discount)
+        return instance
+
 
 #    def clean_quantity(self):
 #        quantity = self.cleaned_data.get('quantity')
-#        discounts = self.instance.discount.all()
-#        if discounts:
-#            can_add = True
-#            allowed_quantity = None
-#            for discount in discounts:
-#                if quantity > discount.allowed_uses:
+#        instance = copy(self.instance)
+#        try:
+#            discount = instance.cart.discount
+#        except Discount.DoesNotExist:
+#            return quantity
+##        discounts = self.instance.discount.all()
+##        if discounts:
+##            can_add = True
+##            allowed_quantity = None
+##            for discount in discounts:
+##                if quantity > discount.allowed_uses:
 #                    can_add = False
 #                    if allowed_quantity is None or \
 #                                    allowed_quantity > discount.allowed_uses:
